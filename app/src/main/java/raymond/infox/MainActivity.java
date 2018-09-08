e2a7d639014ba936e440b534ffb90123550e8961
w0j1b <w0j1b@ugrad.cs.ubc.ca>
2018-09-07 22:04:27 -0700

    Added barcode scanning capability

--- app/src/main/java/raymond/infox/MainActivity.java
+++ app/src/main/java/raymond/infox/MainActivity.java
@@ -1,13 +1,47 @@
 package raymond.infox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    TextView barcodeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        barcodeResult = (TextView)findViewById(R.id.barcode_result);
+    }
+
+    //add a click event on the button
+    public void scanBarcode(View v) {
+        Intent intent = new Intent(this, ScanBarcodeActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    String val = barcode.displayValue;
+                    String result = "Value : " + val ;
+                    barcodeResult.setText(result);
+                } else {
+                    barcodeResult.setText("No barcode detected");
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
