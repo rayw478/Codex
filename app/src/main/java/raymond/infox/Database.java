859c5876b9e290a7b3a9e08e714ae4aa4797ecdd
Raymond Wong <raymondw478@gmail.com>
2019-04-16 13:29:51 -0700

    -Updated icons TODO: - Search - Categories

--- app/src/main/java/raymond/infox/Database.java
+++ app/src/main/java/raymond/infox/Database.java
@@ -22,7 +22,7 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (" + COL1 + " int UNIQUE, " + COL2 +" varchar(255), " + COL3 + " varchar(16))";
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" + COL1 + " varchar(255) UNIQUE, " + COL2 + " varchar(255), " + COL3 + " varchar(16))";
         db.execSQL(createTable);
     }
 
